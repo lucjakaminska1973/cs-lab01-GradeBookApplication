1f8bf36d89d0d9e3b3616866fe1b028bfe98e79a
kamilu <lucja.kaminska@microsoft.wsei.edu.pl>
2020-10-17 20:00:28 +0200

    zmiana kodu 1 push

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    class RankedGradeBook: BaseGradeBook
+    {
+        public RankedGradeBook(string name, bool isWeight) : base(name, isWeight)
+        {
+            Type = GradeBookType.Ranked;
+        }
+        public override void CalculateStatistics()
+        {
+            string students1 = Convert.ToString(Students);
+            int result1 = Students.Count;
+            if (result1 < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
+        public override void CalculateStudentStatistics(string name)
+        {
+            string students1 = Convert.ToString(Students);
+            int result1 = Students.Count;
+            if (result1 < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
+        }
+    }
+}
